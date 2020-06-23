d97cd72e881db47fcc5ba4c82d353590facc6d51
Banu <banu17108.it@rmkec.ac.in>
2020-06-23 18:01:18 +0530

    INITIAL COMMIT

---
+++ ll.js
@@ -0,0 +1,5 @@
+var Boy=prompt("Enter the name of the Boy: ");
+var girl=prompt("Enter the name of the Girl: ");
+var rnumber=Math.random();
+var loverange=Math.floor((rnumber*100))+1;
+console.log("Love between "+Boy+" and "+girl+" is "+loverange+"%");
